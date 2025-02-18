3ede72d7c946117a0b9cbfb587c1bcba7f0683d7
Yingjie Li <yingjieli.edu@gmail.com>
2025-02-17 22:17:30 -0800

    use updated data after double checking in Covidence

---
+++ code/_parameters.R
@@ -0,0 +1,9 @@
+
+
+
+
+## select tools used in experimental studies 
+tool_selected_rct <- c('PANAS', 'POMS', 
+                       'STAI', 'SVS', 
+                       'DASS-21',
+                       'PSS', 'ROS')
